b8d873ff30b387968d7b9ac6d3ab41b131d10c15
Jan Vollmer <jan@vllmr.dev>
2026-01-29 08:24:25 +0100

    fix: apply display style to scheduler entry again

--- src/SchedulerBody/SchedulerBody.tsx
+++ src/SchedulerBody/SchedulerBody.tsx
@@ -1,4 +1,10 @@
-import { Box, Flex, MantineTheme, Paper } from "@mantine/core";
+import {
+  Box,
+  Flex,
+  MantineStyleProps,
+  MantineTheme,
+  Paper,
+} from "@mantine/core";
 import { useWindowVirtualizer } from "@tanstack/react-virtual";
 
 import React, { useMemo, useRef } from "react";
@@ -94,6 +100,10 @@ const SchedulerEntries = <TData, TResource>({
         const startDistance = calculateDistancePercentage(startDate, "left");
         const endDistance = calculateDistancePercentage(endDate, "right");
 
+        const display: MantineStyleProps["display"] = isVisible
+          ? undefined
+          : "none";
+
         const entryId = getDataId(item);
         const offsetMultiplier = entryOffsets[entryId] ?? 0;
         const top = rowHeight * offsetMultiplier + 0.1 * rowHeight;
@@ -105,6 +115,7 @@ const SchedulerEntries = <TData, TResource>({
             <SchedulerEntryRenderer
               CustomSchedulerEntry={entryComponent}
               style={{
+                display,
                 position: "absolute",
                 top,
                 left: `${startDistance}%`,
